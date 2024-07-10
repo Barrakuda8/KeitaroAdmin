d3b56ce2745669cda567b01bbcbd34f50ed6bc58
Barrakuda8 <brpslelush@gmail.com>
2024-07-10 16:50:03 +0300

    Update costs optimization

--- adminapp/management/commands/get_costs.py
+++ adminapp/management/commands/get_costs.py
@@ -1,4 +1,3 @@
-import json
 from datetime import datetime, timedelta
 from pprint import pprint
 
@@ -7,10 +6,10 @@ import requests
 from django.core.management import BaseCommand
 
 import config
-from adminapp.models import Account, Cost, Cabinet, Campaign, Ad, AdSet, Action, Update
+from adminapp.models import Account, Cost, Cabinet, Update
 
 
-def get_data(fbtool_id, date, cabinet=None):
+def get_data(fbtool_id, date):
     params = {
         "key": config.FBTOOL_KEY,
         "account": fbtool_id,
@@ -21,99 +20,6 @@ def get_data(fbtool_id, date, cabinet=None):
     }
     response = requests.get('https://fbtool.pro/api/get-statistics/', params=params)
     return response.json()
-
-
-def process_data(cabinet_data, date, currencies, definitive=False):
-    updated_campaigns = {}
-    updated_adsets = {}
-    cabinet_id = int(cabinet_data['account_id'])
-    cabinet = Cabinet.objects.get(pk=cabinet_id)
-    for ad_data in cabinet_data['ads']['data']:
-
-        campaign_id = int(ad_data['campaign']['id'])
-        if campaign_id not in updated_campaigns.keys():
-            campaign_check = Campaign.objects.filter(pk=campaign_id)
-            campaign_data = ad_data['campaign']
-            if campaign_check.exists():
-                campaign = campaign_check.first()
-                campaign.name = campaign_data['name']
-                campaign.status = campaign_data['status']
-                campaign.effective_status = campaign_data['effective_status']
-                campaign.save()
-                updated_campaigns[campaign_id] = campaign
-            else:
-                campaign_data['cabinet'] = cabinet
-                campaign = Campaign.objects.create(**campaign_data)
-        else:
-            campaign = updated_campaigns[campaign_id]
-
-        adset_id = int(ad_data['adset']['id'])
-        if adset_id not in updated_adsets.keys():
-            adset_check = AdSet.objects.filter(pk=adset_id)
-            adset_data = ad_data['adset']
-            if adset_check.exists():
-                adset = adset_check.first()
-                adset.name = adset_data['name']
-                adset.status = adset_data['status']
-                adset.effective_status = adset_data['effective_status']
-                adset.save()
-                updated_adsets[adset_id] = adset
-            else:
-                adset_data['campaign'] = campaign
-                adset = AdSet.objects.create(**adset_data)
-        else:
-            adset = updated_adsets[adset_id]
-
-        ad_id = int(ad_data['id'])
-        ad_check = Ad.objects.filter(pk=ad_id)
-
-        insights = None
-        if 'insights' in ad_data.keys():
-            insights = ad_data['insights']['data']
-            del ad_data['insights']
-        if ad_check.exists():
-            ad = ad_check.first()
-            ad.name = ad_data['name']
-            ad.status = ad_data['status']
-            ad.effective_status = ad_data['effective_status']
-            ad.creative = ad_data['creative']
-            ad.save()
-        else:
-            ad_data['adset'] = adset
-            del ad_data['campaign']
-            ad = Ad.objects.create(**ad_data)
-
-        if insights is not None:
-            Cost.objects.filter(ad__pk=ad.pk, date=date).delete()
-
-            for insight in insights:
-                actions = []
-                cpat = []
-
-                if 'actions' in insight.keys():
-                    actions = insight['actions']
-                    del insight['actions']
-                if 'cost_per_action_type' in insight.keys():
-                    cpat = insight['cost_per_action_type']
-                    del insight['cost_per_action_type']
-
-                insight['ad'] = ad
-                insight['date'] = date
-                insight['definitive'] = definitive
-                insight['amount'] = insight['spend']
-                insight['amount_USD'] = round(float(insight['amount']) * currencies[cabinet.currency.lower()]['inverseRate'], 2) if cabinet.currency != 'USD' else insight['amount']
-                del insight['spend']
-                del insight['date_start']
-                del insight['date_stop']
-                cost = Cost.objects.create(**insight)
-
-                actions_to_create = {a['action_type']: {'count': a['value'], 'type': a['action_type'], 'cost': cost}
-                                     for a in actions}
-                for action in cpat:
-                    actions_to_create[action['action_type']]['value'] = action['value']
-
-                for action in actions_to_create.values():
-                    Action.objects.create(**action)
 
 
 class Command(BaseCommand):
@@ -167,17 +73,17 @@ class Command(BaseCommand):
                                                and Cost.objects.filter(date=yesterday, definitive=False,
                                                                        ad__adset__campaign__cabinet__pk=cabinet.pk).exists()))
                             and str_cabinet_pk in yesterday_data.keys() and 'ads' in yesterday_data[str_cabinet_pk].keys()):
-                        process_data(yesterday_data[str_cabinet_pk], yesterday, currencies, date == today)
+                        cabinet.update_costs(cabinet_data=yesterday_data[str_cabinet_pk], currencies=currencies)
 
                     if ((date == today or (date == tomorrow
                                            and Cost.objects.filter(date=today, definitive=False,
                                                                    ad__adset__campaign__cabinet__pk=cabinet.pk).exists()))
                             and str_cabinet_pk in today_data.keys() and 'ads' in today_data[str_cabinet_pk].keys()):
-                        process_data(today_data[str_cabinet_pk], today, currencies, date == tomorrow)
+                        cabinet.update_costs(cabinet_data=today_data[str_cabinet_pk], currencies=currencies)
 
                     if (date == tomorrow and str_cabinet_pk in tomorrow_data.keys()
                             and 'ads' in tomorrow_data[str_cabinet_pk].keys()):
-                        process_data(tomorrow_data[str_cabinet_pk], tomorrow, currencies)
+                        cabinet.update_costs(cabinet_data=tomorrow_data[str_cabinet_pk], currencies=currencies)
 
         update.finished = True
         update.save()
--- adminapp/models.py
+++ adminapp/models.py
@@ -1,6 +1,12 @@
 from datetime import datetime, timedelta
+from pprint import pprint
 
+import pytz
+import requests
 from django.db import models
+from django.dispatch import receiver
+
+import config
 from authapp.models import User
 
 
@@ -70,9 +76,120 @@ class Cabinet(models.Model):
             if not update.finished and datetime.now() >= update.datetime + timedelta(minutes=1):
                 update.finished = True
                 update.save()
-            print(update.finished)
             return update.finished
         return True
+
+    def update_costs(self, date_start=None, date_stop=None, cabinet_data=None, currencies=None):
+        update = Update.objects.create(type=f'cabinet-costs-{self.pk}', datetime=datetime.now(), finished=False)
+        if cabinet_data is None:
+            params = {
+                "key": config.FBTOOL_KEY,
+                "account": self.account.fbtool_id,
+                "dates": f"{date_start} - {date_stop}",
+                "mode": "ads",
+                "status": "all",
+                "byDay": 1,
+                "ad_account": self.pk
+            }
+            response = requests.get('https://fbtool.pro/api/get-statistics/', params=params)
+            cabinet_data = response.json()['data'][0]
+
+        if currencies is None:
+            currencies = requests.get('https://www.floatrates.com/daily/usd.json').json()
+        currency_rate = currencies[self.currency.lower()]['inverseRate']
+        if 'ads' in cabinet_data.keys():
+            updated_campaigns = {}
+            updated_adsets = {}
+            for ad_data in cabinet_data['ads']['data']:
+                campaign_id = int(ad_data['campaign']['id'])
+                if campaign_id not in updated_campaigns.keys():
+                    campaign_check = Campaign.objects.filter(pk=campaign_id)
+                    campaign_data = ad_data['campaign']
+                    if campaign_check.exists():
+                        campaign = campaign_check.first()
+                        campaign.name = campaign_data['name']
+                        campaign.status = campaign_data['status']
+                        campaign.effective_status = campaign_data['effective_status']
+                        campaign.save()
+                        updated_campaigns[campaign_id] = campaign
+                    else:
+                        campaign_data['cabinet'] = self
+                        campaign = Campaign.objects.create(**campaign_data)
+                else:
+                    campaign = updated_campaigns[campaign_id]
+
+                adset_id = int(ad_data['adset']['id'])
+                if adset_id not in updated_adsets.keys():
+                    adset_check = AdSet.objects.filter(pk=adset_id)
+                    adset_data = ad_data['adset']
+                    if adset_check.exists():
+                        adset = adset_check.first()
+                        adset.name = adset_data['name']
+                        adset.status = adset_data['status']
+                        adset.effective_status = adset_data['effective_status']
+                        adset.save()
+                        updated_adsets[adset_id] = adset
+                    else:
+                        adset_data['campaign'] = campaign
+                        adset = AdSet.objects.create(**adset_data)
+                else:
+                    adset = updated_adsets[adset_id]
+
+                ad_id = int(ad_data['id'])
+                ad_check = Ad.objects.filter(pk=ad_id)
+                insights = None
+                if 'insights' in ad_data.keys():
+                    insights = ad_data['insights']['data']
+                    del ad_data['insights']
+                if ad_check.exists():
+                    ad = ad_check.first()
+                    ad.name = ad_data['name']
+                    ad.status = ad_data['status']
+                    ad.effective_status = ad_data['effective_status']
+                    ad.creative = ad_data['creative']
+                    ad.save()
+                else:
+                    ad_data['adset'] = adset
+                    del ad_data['campaign']
+                    ad = Ad.objects.create(**ad_data)
+                if insights is not None:
+                    for insight in insights:
+                        cost_check = Cost.objects.filter(ad__pk=ad.pk, date=insight['date_start'])
+                        if not cost_check.exists() or not cost_check.first().definitive:
+                            cost_check.delete()
+                            actions = []
+                            cpat = []
+
+                            if 'actions' in insight.keys():
+                                actions = insight['actions']
+                                del insight['actions']
+                            if 'cost_per_action_type' in insight.keys():
+                                cpat = insight['cost_per_action_type']
+                                del insight['cost_per_action_type']
+
+                            insight['ad'] = ad
+                            insight['date'] = insight['date_start']
+                            insight['amount'] = insight['spend']
+                            insight['amount_USD'] = round(float(insight['amount']) * currency_rate,
+                                                          2) if self.currency != 'USD' else insight['amount']
+                            if (datetime.strptime(insight['date_start'], '%Y-%m-%d').date()
+                                    < datetime.now(pytz.timezone(self.timezone)).date()):
+                                insight['definitive'] = True
+                            del insight['spend']
+                            del insight['date_start']
+                            del insight['date_stop']
+                            cost = Cost.objects.create(**insight)
+
+                            actions_to_create = {
+                                a['action_type']: {'count': a['value'], 'type': a['action_type'], 'cost': cost}
+                                for a in actions}
+                            for action in cpat:
+                                actions_to_create[action['action_type']]['value'] = action['value']
+
+                            for action in actions_to_create.values():
+                                Action.objects.create(**action)
+        update.finished = True
+        update.save()
 
 
 class Campaign(models.Model):
@@ -170,3 +287,11 @@ class Revenue(models.Model):
     os_icon = models.TextField(null=True, blank=True)
     os_version = models.TextField(null=True, blank=True)
     definitive = models.BooleanField(default=False, verbose_name='Окончательный вариант')
+
+
+@receiver(models.signals.post_save, sender=Cabinet)
+def update_costs_signal(sender, instance, created, **kwargs):
+    if created:
+        date_stop = datetime.now().date()
+        date_start = date_stop - timedelta(days=30)
+        instance.update_costs(date_start, date_stop)
